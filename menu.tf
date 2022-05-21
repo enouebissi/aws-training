bf91a0f381a0414fa42871ba0d8abf6a0761ba25
emmanuel nouebissi <enouebissi@gmail.com>
2022-05-20 21:51:24 -0400

    added subnet

--- menu.tf
+++ menu.tf
@@ -1,9 +1,16 @@
 provider "aws" {
-  region = us-west-2
+  region = "us-west-2"
 }
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.1.0/24"
   tags = {
     Name = "my-vpc"
+  }
+}
+resource "aws_subnet" "my_sbnt" {
+  cidr_block = "10.0.1.0/25"
+  vpc_id     = aws_vpc.my_vpc.id
+  tags = {
+    "Name" = "my-sbnt"
   }
 }
